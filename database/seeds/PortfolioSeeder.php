5738396b73c919fa9ea1a6ee4e19b487011836f5
Leo <hello@leojr.me>
2020-01-17 19:23:06 -0500

    /

--- database/seeds/PortfolioSeeder.php
+++ database/seeds/PortfolioSeeder.php
@@ -17,7 +17,7 @@ class PortfolioSeeder extends Seeder
             'title' => 'ISA',
             'type' => 'web_development',
             'website_url' => 'https://isaclean.ca',
-            'description' => '<p>Technologies implemented:</p>,
+            'description' => '<p>Technologies implemented:</p>
             <ul>
                 <li>PHP</li>
                 <li>Laravel</li>
@@ -28,6 +28,61 @@ class PortfolioSeeder extends Seeder
                 <li>JavaScript</li>
             </ul>',
             'image' => '1579151841.png',
+        ]);      
+
+        Portfolio::create([
+            'user_id' => 1,
+            'title' => 'Sacred Light Healing Centre',
+            'type' => 'web_development',
+            'website_url' => 'https://sacredlighthealing.ca',
+            'description' => '<p>Technologies implemented:</p>
+            <ul>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Bootstrap</li>
+                <li>SASS</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+                <li>Vue.js</li>
+            </ul>',
+            'image' => '1579297874.png',
+        ]);      
+        Portfolio::create([
+            'user_id' => 1,
+            'title' => 'Triple C Automotive',
+            'type' => 'web_development',
+            'website_url' => 'https://triplecauto.ca',
+            'description' => '<p>Technologies implemented:</p>
+            <ul>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Bootstrap</li>
+                <li>SASS</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+                <li>Vue.js</li>
+            </ul>',
+            'image' => '1579306599.png',
+        ]);      
+        Portfolio::create([
+            'user_id' => 1,
+            'title' => 'Wonderful Works Of Jesus',
+            'type' => 'web_development',
+            'website_url' => 'https://wonderfulworksofjesus.com',
+            'description' => '<p>Technologies implemented:</p>
+            <ul>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>Bootstrap</li>
+                <li>SASS</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+                <li>Vue.js</li>
+            </ul>',
+            'image' => '1579306615.png',
         ]);      
     }
 }
